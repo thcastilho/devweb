2ceeded5f6edc03ca4f9c98cf57ad95900496e2b
thcastilho <thiagocesaralmeida@gmail.com>
2024-04-22 23:02:14 -0300

    Apresentação Parcial
    Código utilizado na apresentação parcial do projeto.

--- api/src/main/java/br/unesp/rc/app/repository/ProdutoRepository.java
+++
@@ -1,9 +0,0 @@
-package br.unesp.rc.app.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import br.unesp.rc.app.model.Produto;
-
-public interface ProdutoRepository extends CrudRepository<Produto, Long>{
-    
-}
