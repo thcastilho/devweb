87e5e9c31d3380556ead32d7a4812f37d28405ec
thcastilho <thiagocesaralmeida@gmail.com>
2024-05-17 12:18:36 -0300

    consertando CrossOrigin para criar um Post

--- api/src/main/java/br/unesp/rc/app/controller/PostController.java
+++ api/src/main/java/br/unesp/rc/app/controller/PostController.java
@@ -10,6 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -40,7 +41,7 @@ public class PostController {
     } 
 
     // Cria novo post
-    @CrossOrigin("http:localhost:3000")
+    @CrossOrigin("http://localhost:3000")
     @PostMapping(value = "/", produces = "application/json")
     public ResponseEntity<Post> createPost(@RequestHeader (name="Authorization") String token, @RequestBody Post post) {
         Post postSalvo = postService.createPost(token, post);
