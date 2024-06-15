0ba62937b7b77abb6ca5f2211551978123a5a580
thcastilho <thiagocesaralmeida@gmail.com>
2024-06-15 16:47:58 -0300

    correção - deleção de Post e Gênero

--- api/src/main/resources/schema.sql
+++
@@ -1,6 +0,0 @@
--- possibilitar deleção de gêneros em cascata
-ALTER TABLE posts_generos
-DROP FOREIGN KEY FKr10k9p0c74mg95cf2svjnb2cf;
-
-ALTER TABLE posts_generos
-ADD CONSTRAINT FKr10k9p0c74mg95cf2svjnb2cf FOREIGN KEY (genero_id) REFERENCES generos(id) ON DELETE CASCADE;
